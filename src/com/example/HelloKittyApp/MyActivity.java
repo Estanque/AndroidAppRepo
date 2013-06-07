725b71086d0f9911c45f0e7f25dc5aa708aa1fb1
Andrew Rayn <andrewrayn>
2013-06-07 10:15:49 +0600

    Light Traffic App
    Светофор. Приложение изменяет фон и надпись.

--- src/com/example/HelloKittyApp/MyActivity.java
+++ src/com/example/HelloKittyApp/MyActivity.java
@@ -3,27 +3,34 @@ package com.example.HelloKittyApp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
-	TextView tvHello;
-	Boolean trigger = true;
-
+	public TextView tvInfo;
+	public RelativeLayout relativeLayout;
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		tvHello = (TextView) findViewById(R.id.textView1);
+		tvInfo = (TextView) findViewById(R.id.textView);
+		relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 	}
 
-	public  void onClick(View v){
-		if (trigger) {
-			tvHello.setText(R.string.helloText);
-		} else {
-			tvHello.setText(R.string.app_name);
-		}
-		trigger=!trigger;
+	public void buttonGreen_Click(View v){
+		tvInfo.setText(R.string.green);
+		relativeLayout.setBackgroundResource(R.color.greenColor);
+	}
+	
+	public void buttonYellow_Click(View v){
+		tvInfo.setText(R.string.yellow);
+		relativeLayout.setBackgroundResource(R.color.yellowColor);
+	}
+	
+	public void buttonRed_Click(View v){
+		tvInfo.setText(R.string.red);
+		relativeLayout.setBackgroundResource(R.color.redColor);
 	}
 }
