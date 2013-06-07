474e822c0a3c43d396c53ca0863292a658fa9760
Estanque <arayn642@gmail.com>
2013-06-07 12:01:10 +0600

    Sending message to Activity

--- src/com/example/HelloKittyApp/MyActivity.java
+++ src/com/example/HelloKittyApp/MyActivity.java
@@ -4,25 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MyActivity extends Activity {
-	public TextView tvInfo;
-	public RelativeLayout relativeLayout;
+	EditText edUserName;
+	EditText edDescription;
+	EditText edAuthor;
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		tvInfo = (TextView) findViewById(R.id.textView);
-		relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+		edUserName = (EditText)findViewById(R.id.edUserName);
+		edDescription = (EditText)findViewById(R.id.edDescription);
+		edAuthor = (EditText)findViewById(R.id.edAuthor);
 	}
 
 	public void buttonClick(View v){
 		switch (v.getId()){
 			case R.id.buttonOne:
-				Intent intent = new Intent(MyActivity.this,AboutActivity.class);
+				Intent intent = new Intent(MyActivity.this,HelloActivity.class);
+				intent.putExtra("username",edUserName.getText().toString());
+				intent.putExtra("gift",edDescription.getText().toString());
+				intent.putExtra("author",edAuthor.getText().toString());
 				startActivity(intent);
 				break;
 
