a3c5b6c0393adc47ec71abb1bbaad1f35ac11c17
Andrew Rayn <andrewrayn>
2013-06-07 09:21:04 +0600

    Second App
    Приложение для подсчета ворон и котов.
    Тест.

--- src/com/example/HelloKittyApp/MyActivity.java
+++ src/com/example/HelloKittyApp/MyActivity.java
@@ -3,12 +3,15 @@ package com.example.HelloKittyApp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
 	TextView tvHello;
 	Boolean trigger = true;
+	int crowsCounter = 0;
+	int catsCounter = 0;
 
 
 	@Override
@@ -16,14 +19,17 @@ public class MyActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		tvHello = (TextView) findViewById(R.id.textView1);
+		Button catsButton = (Button) findViewById(R.id.buttonCats);
+		catsButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick (View view) {
+				tvHello.setText("I count "+ ++catsCounter +" cats!");
+			}
+		});
 	}
 
-	public  void onClick(View v){
-		if (trigger) {
-			tvHello.setText(R.string.helloText);
-		} else {
-			tvHello.setText(R.string.app_name);
-		}
-		trigger=!trigger;
+	public void buttonCrows_Click(View v){
+		TextView textView = (TextView) findViewById(R.id.textView1);
+		textView.setText("I count "+ ++crowsCounter +" crows!");
 	}
 }
