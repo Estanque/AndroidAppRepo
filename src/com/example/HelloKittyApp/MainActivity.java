4874373cc51205c1b89e24038ea0a03c60ea1aab
Estanque <arayn642@gmail.com>
2013-06-19 09:26:18 +0600

    Reading action from Intent

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,57 +1,39 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends Activity
-		implements SeekBar.OnSeekBarChangeListener {
-
-	SeekBar sbWeight;
-	Button btn1;
-	Button btn2;
-
-	LinearLayout.LayoutParams lParams1;
-	LinearLayout.LayoutParams lParams2;
+							implements View.OnClickListener {
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		sbWeight = (SeekBar) findViewById(R.id.sbWeight);
-		// Setup Listeners on SeekBar (!NECESSARY)
-		sbWeight.setOnSeekBarChangeListener(this);
+		Button btnTime = (Button) findViewById(R.id.btnTime);
+		Button btnDate = (Button) findViewById(R.id.btnDate);
 
-		btn1 = (Button) findViewById(R.id.btn1);
-		btn2 = (Button) findViewById(R.id.btn2);
-
-		lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
-		lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+		btnTime.setOnClickListener(this);
+		btnDate.setOnClickListener(this);
 	}
 
 	@Override
-	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-		int leftValue = progress;
-		int rightValue = seekBar.getMax() - progress;
-		// Setup weight
-		lParams1.weight = leftValue;
-		lParams2.weight = rightValue;
-		// Write values to button.text
-		btn1.setText(String.valueOf(leftValue));
-		btn2.setText(String.valueOf(rightValue));
-	}
+	public void onClick(View view) {
+		Intent intent;
 
-	@Override
-	public void onStartTrackingTouch(SeekBar seekBar) {
-		//To change body of implemented methods use File | Settings | File Templates.
-	}
-
-	@Override
-	public void onStopTrackingTouch(SeekBar seekBar) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		switch (view.getId()){
+			case R.id.btnTime:
+				intent = new Intent("com.example.intent.action.showtime");
+				startActivity(intent);
+				break;
+			case R.id.btnDate:
+				intent = new Intent("com.example.intent.action.showdate");
+				startActivity(intent);
+				break;
+		}
 	}
 }
