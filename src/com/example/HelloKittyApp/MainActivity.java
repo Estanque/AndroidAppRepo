0f572b345d7b04b8b3ee84d4e5d1b225634af648
Estanque <arayn642@gmail.com>
2013-06-20 13:05:16 +0600

    ListView example. Simple code

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,53 +1,31 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
 
-	String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
-				 "Костя", "Игорь" };
-	String[] position = { "Программер", "Бухгалтер", "Программер",
-				 "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" };
-	int[] salary = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
-
-	int[] colors = new int[2];
+	String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+					  "Костя", "Игорь" };
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		colors[0] = Color.parseColor("#559966CC");
-		colors[1] = Color.parseColor("#55336699");
+		//find list
+		ListView lvMain = (ListView) findViewById(R.id.lvMain);
 
-		LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+		//create adapter
+		//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		//		android.R.layout.simple_list_item_1, names);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+						R.layout.my_list_item, names);
 
-		LayoutInflater inflater = getLayoutInflater();
-
-		for (int i = 0; i < name.length; i++) {
-			View item = inflater.inflate(R.layout.item,linLayout,false);
-
-			TextView tvName = (TextView) item.findViewById(R.id.tvName);
-			TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
-			TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
-
-			tvName.setText(name[i]);
-			tvPosition.setText("Должность: " + position[i]);
-			tvSalary.setText("Оклад: " + salary[i]);
-
-			item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
-			item.setBackgroundColor(colors[i % 2]);
-			linLayout.addView(item);
-		}
+		//assign adapter to List
+		lvMain.setAdapter(adapter);
 	}
 }
