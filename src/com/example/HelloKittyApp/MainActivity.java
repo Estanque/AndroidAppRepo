4a1ca875422dfd7d05a67dedaca0e7cbca6c3493
Estanque <arayn642@gmail.com>
2013-06-20 11:06:07 +0600

    Inflator exaple (with bugs)

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,77 +1,42 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.*;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends Activity
-							implements View.OnClickListener{
 
-	Button btnSave, btnLoad;
-	EditText etText;
+public class MainActivity extends Activity {
 
-	SharedPreferences sPref;
-
-	final String SAVED_TEXT = "saved_text";
+	final String LOG_TAG = "myLogs";
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		etText = (EditText) findViewById(R.id.edText);
+		LayoutInflater inflater = getLayoutInflater();
 
-		btnSave = (Button) findViewById(R.id.btnSave);
-		btnLoad = (Button) findViewById(R.id.btnLoad);
+		LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+		View view1 = inflater.inflate(R.layout.text, linLayout, true);
+		ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
 
-		btnLoad.setOnClickListener(this);
-		btnSave.setOnClickListener(this);
+		Log.d(LOG_TAG, "Class of view: " + view1.getClass().toString());
+		Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp1 == null));
+		Log.d(LOG_TAG, "Text of view: " + ((TextView) view1).getText());
 
-		loadText();
-	}
+		RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+		View view2 = inflater.inflate(R.layout.text,relLayout,false);
+		ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
+		relLayout.addView(view2);
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()){
-			case R.id.btnSave:
-				saveText();
-				break;
-			case R.id.btnLoad:
-				loadText();
-				break;
-			default:
-				break;
-		}
-	}
-
-	private void saveText() {
-		sPref = getPreferences(MODE_PRIVATE);
-		SharedPreferences.Editor ed = sPref.edit();
-		ed.putString(SAVED_TEXT,etText.getText().toString());
-		ed.commit();
-		Toast.makeText(getApplicationContext(),
-				"Text Saved",
-				Toast.LENGTH_SHORT).show();
-	}
-
-	private void loadText() {
-		sPref = getPreferences(MODE_PRIVATE);
-		String savedText = sPref.getString(SAVED_TEXT,"");
-		etText.setText(savedText);
-		Toast.makeText(getApplicationContext(),
-				"Text loaded",
-				Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	protected void onDestroy(){
-		saveText();
-		super.onDestroy();
+		Log.d(LOG_TAG, "Class of view: " + view2.getClass().toString());
+		Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp2 == null));
+		Log.d(LOG_TAG, "Text of view: " + ((TextView) view2).getText());
 	}
 }
