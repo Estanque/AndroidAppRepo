3352d0f1e2654ab0ff567e29909bb7e7b82e9ff6
Estanque <arayn642@gmail.com>
2013-06-20 11:42:36 +0600

    Inflator example. Analog ListView with massive

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,30 +14,40 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	final String LOG_TAG = "myLogs";
+	String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+				 "Костя", "Игорь" };
+	String[] position = { "Программер", "Бухгалтер", "Программер",
+				 "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" };
+	int[] salary = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
+
+	int[] colors = new int[2];
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		LayoutInflater inflater = getLayoutInflater();
+		colors[0] = Color.parseColor("#559966CC");
+		colors[1] = Color.parseColor("#55336699");
 
 		LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-		View view1 = inflater.inflate(R.layout.text, linLayout, true);
-		ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
 
-		Log.d(LOG_TAG, "Class of view: " + view1.getClass().toString());
-		Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp1 == null));
-		Log.d(LOG_TAG, "Text of view: " + ((TextView) view1).getText());
+		LayoutInflater inflater = getLayoutInflater();
 
-		RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
-		View view2 = inflater.inflate(R.layout.text,relLayout,false);
-		ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
-		relLayout.addView(view2);
+		for (int i = 0; i < name.length; i++) {
+			View item = inflater.inflate(R.layout.item,linLayout,false);
 
-		Log.d(LOG_TAG, "Class of view: " + view2.getClass().toString());
-		Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp2 == null));
-		Log.d(LOG_TAG, "Text of view: " + ((TextView) view2).getText());
+			TextView tvName = (TextView) item.findViewById(R.id.tvName);
+			TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+			TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+
+			tvName.setText(name[i]);
+			tvPosition.setText("Должность: " + position[i]);
+			tvSalary.setText("Оклад: " + salary[i]);
+
+			item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+			item.setBackgroundColor(colors[i % 2]);
+			linLayout.addView(item);
+		}
 	}
 }
