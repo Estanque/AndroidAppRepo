4359afa535721c3074124a41c3540dde7f5b231b
Estanque <arayn642@gmail.com>
2013-06-21 17:48:01 +0600

    Advanced SimpleAdapter

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,54 +1,94 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends Activity {
 
+	// имена атрибутов для Map
 	final String ATTRIBUTE_NAME_TEXT = "text";
-	final String ATTRIBUTE_NAME_CHECKED = "checked";
+	final String ATTRIBUTE_NAME_VALUE = "value";
 	final String ATTRIBUTE_NAME_IMAGE = "image";
+
+	// картинки для отображения динамики
+	final int positive = android.R.drawable.arrow_up_float;
+	final int negative = android.R.drawable.arrow_down_float;
 
 	ListView lvSimple;
 
-	@Override
-	public void onCreate (Bundle savedInstanceState) {
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		//massive of data
-		String[] texts = { "sometext 1", "sometext 2", "sometext 3",
-				"sometext 4", "sometext 5" };
-		boolean[] checked = { true, false, false, true, false };
-		int img = R.drawable.ic_launcher;
+		// массив данных
+		int[] values = { 8, 4, -3, 2, -5, 0, 3, -6, 1, -1 };
 
-		//packing data to structure
-		ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(texts.length);
+		// упаковываем данные в понятную для адаптера структуру
+		ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(
+				values.length);
 		Map<String, Object> m;
-		for (int i = 0; i < texts.length; i++) {
+		int img = 0;
+		for (int i = 0; i < values.length; i++) {
 			m = new HashMap<String, Object>();
-			m.put(ATTRIBUTE_NAME_TEXT, texts[i]);
-			m.put(ATTRIBUTE_NAME_CHECKED, checked[i]);
-			m.put(ATTRIBUTE_NAME_IMAGE,img);
+			m.put(ATTRIBUTE_NAME_TEXT, "Day " + (i + 1));
+			m.put(ATTRIBUTE_NAME_VALUE, values[i]);
+			if (values[i] == 0) img = 0; else
+				img = (values[i] > 0) ? positive : negative;
+			m.put(ATTRIBUTE_NAME_IMAGE, img);
 			data.add(m);
 		}
 
 		// массив имен атрибутов, из которых будут читаться данные
-		String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_CHECKED, ATTRIBUTE_NAME_IMAGE, ATTRIBUTE_NAME_TEXT};
-
+		String[] from = { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_VALUE,
+				ATTRIBUTE_NAME_IMAGE };
 		// массив ID View-компонентов, в которые будут вставлять данные
-		int[] to = {R.id.tvText,R.id.cbCheckbox, R.id.ivImg, R.id.cbCheckbox};
+		int[] to = { R.id.tvText, R.id.tvValue, R.id.ivImg };
 
-		// create adapter
-		SimpleAdapter simpleAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+		// создаем адаптер
+		MySimpleAdapter sAdapter = new MySimpleAdapter(this, data,
+				R.layout.item, from, to);
 
-		// define list and assign adapter to it
+		// определяем список и присваиваем ему адаптер
 		lvSimple = (ListView) findViewById(R.id.lvSimple);
-		lvSimple.setAdapter(simpleAdapter);
+		lvSimple.setAdapter(sAdapter);
+	}
+
+	class MySimpleAdapter extends SimpleAdapter {
+
+		public MySimpleAdapter(Context context,
+		                       List<? extends Map<String, ?>> data, int resource,
+		                       String[] from, int[] to) {
+			super(context, data, resource, from, to);
+		}
+
+		@Override
+		public void setViewText(TextView v, String text) {
+			// метод супер-класса, который вставляет текст
+			super.setViewText(v, text);
+			// если нужный нам TextView, то разрисовываем
+			if (v.getId() == R.id.tvValue) {
+				int i = Integer.parseInt(text);
+				if (i < 0) v.setTextColor(Color.RED); else
+				if (i > 0) v.setTextColor(Color.GREEN);
+			}
+		}
+
+		@Override
+		public void setViewImage(ImageView v, int value) {
+			// метод супер-класса
+			super.setViewImage(v, value);
+			// разрисовываем ImageView
+			if (value == negative) v.setBackgroundColor(Color.RED); else
+			if (value == positive) v.setBackgroundColor(Color.GREEN);
+		}
 	}
 }
