5969806a0a8002c146d78794cd06ea62cbb127d9
Andrew <arayn642@gmail.com>
2013-06-21 11:56:51 +0600

    ListView example with selection and scrool

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,28 +15,50 @@ public class MainActivity extends Activity {
 	final  String LOG_TAG = "myLogs";
 
 	ListView lvMain;
-	String[] names;
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		//find list
 		lvMain = (ListView) findViewById(R.id.lvMain);
-		//setting choice mode
-		lvMain.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		//create adapter
+
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-				this,R.array.names,
-				android.R.layout.simple_list_item_single_choice);
+				this, R.array.names, android.R.layout.simple_list_item_1);
 		lvMain.setAdapter(adapter);
 
-		//getting massive from resources
-		names = getResources().getStringArray(R.array.names);
-	}
+		lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+				Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+						+ id);
+			}
+		});
 
-	public void onClick(View v){
-		Log.d(LOG_TAG,names[lvMain.getCheckedItemPosition()]);
+		lvMain.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+				Log.d(LOG_TAG, "itemSelect: position = " + position + ", id = "
+						+ id);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+				Log.d(LOG_TAG, "itemSelect: nothing");
+			}
+		});
+
+		lvMain.setOnScrollListener(new AbsListView.OnScrollListener() {
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				Log.d(LOG_TAG, "scrollState = " + scrollState);
+			}
+
+			public void onScroll(AbsListView view, int firstVisibleItem,
+			                     int visibleItemCount, int totalItemCount) {
+				//Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
+				//    + ", visibleItemCount" + visibleItemCount
+				//    + ", totalItemCount" + totalItemCount);
+			}
+		});
 	}
 }
