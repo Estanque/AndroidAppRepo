3b7bd4225fbdca9abcad0a6f1636ff03c91cf42c
Estanque <arayn642@gmail.com>
2013-06-22 13:58:35 +0600

    Using Spinner

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,55 +1,45 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
-import java.util.ArrayList;
-
-
 /*
-http://startandroid.ru/ru/uroki/vse-uroki-spiskom/113-urok-54-kastomizatsija-spiska-sozdaem-svoj-adapter.html
+http://startandroid.ru/ru/uroki/vse-uroki-spiskom/114-urok-55-header-i-footer-v-spiskah-headerviewlistadapter.html
  */
 
 public class MainActivity extends Activity {
 
-	ArrayList<Product> products = new ArrayList<Product>();
-	BoxAdapter boxAdapter;
+	String[] data = {"one", "two", "three", "four", "five"};
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// creating adapter
-		fillData();
-		boxAdapter = new BoxAdapter(this, products);
+		// adapter
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,data);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-		// setup list
-		ListView lvMain = (ListView) findViewById(R.id.lvMain);
-		lvMain.setAdapter(boxAdapter);
-	}
-
-	// generate data for adapter
-	private void fillData() {
-		for (int i = 0; i < 21; i++) {
-			products.add(new Product("Product" + i, i * 1000,
-					R.drawable.ic_launcher,false));
-		}
-	}
-
-	public void showResult(View v){
-		String result = "Goods in box:";
-		for (Product p : boxAdapter.getBox()){
-			if (p.box) {
-				result += "\n" + p.name;
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		spinner.setAdapter(adapter);
+		// Title
+		spinner.setPrompt("Title");
+		//select item
+		spinner.setSelection(2);
+		// setup listener
+		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+				Toast.makeText(getBaseContext(),
+						"position " + i,
+						Toast.LENGTH_SHORT).show();
 			}
-		}
-		Toast.makeText(this,
-				result,
-				Toast.LENGTH_SHORT).show();
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+				//To change body of implemented methods use File | Settings | File Templates.
+			}
+		});
 	}
 }
