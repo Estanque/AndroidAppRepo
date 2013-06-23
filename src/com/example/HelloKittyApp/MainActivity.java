49f4290c40d7096de789f9f8fdc22836fe79c914
Estanque <arayn642@gmail.com>
2013-06-23 10:52:07 +0600

    DatePickerDialog example

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.HelloKittyApp;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
@@ -13,35 +14,37 @@ http://startandroid.ru/ru/uroki/vse-uroki-spiskom/117-urok-58-dialogi-timepicker
 
 public class MainActivity extends Activity {
 
-	int DIALOG_TIME = 1;
-	int myHour = 14;
-	int myMinute = 35;
-	TextView tvTime;
+	int DIALOG_DATE = 1;
+	int myYear = 2011;
+	int myMouth = 02;
+	int myDay = 03;
+	TextView tvDate;
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		tvTime = (TextView) findViewById(R.id.tvTime);
+		tvDate = (TextView) findViewById(R.id.tvDate);
 	}
 
 	public void onclick(View v){
-		showDialog(DIALOG_TIME);
+		showDialog(DIALOG_DATE);
 	}
 
 	protected Dialog onCreateDialog(int id){
-		if (id == DIALOG_TIME) {
-			TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
-			return tpd;
+		if (id == DIALOG_DATE) {
+			DatePickerDialog dpd = new DatePickerDialog(this, myCallBack, myYear, myMouth, myDay);
+			return dpd;
 		}
 		return super.onCreateDialog(id);
 	}
 
-	TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+	DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
 		@Override
-		public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-			myHour = hourOfDay;
-			myMinute = minute;
-			tvTime.setText("Time is " + myHour + ":" + myMinute);
+		public void onDateSet(DatePicker datePicker, int i, int i2, int i3) {
+			myYear = i;
+			myMouth = i2;
+			myDay = i3;
+			tvDate.setText("Today is " + myMouth + "/" + myDay + "/" + myYear);
 		}
 	};
 }
