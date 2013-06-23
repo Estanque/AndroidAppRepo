f0d439426ff6ed50f4ae8bdf11933ecf00da8719
Estanque <arayn642@gmail.com>
2013-06-23 10:30:10 +0600

    GridView example

--- src/com/example/HelloKittyApp/MainActivity.java
+++ src/com/example/HelloKittyApp/MainActivity.java
@@ -11,35 +11,27 @@ http://startandroid.ru/ru/uroki/vse-uroki-spiskom/114-urok-55-header-i-footer-v-
 
 public class MainActivity extends Activity {
 
-	String[] data = {"one", "two", "three", "four", "five"};
+	String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" , "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "q", "x", "y", "z"};
+
+	GridView gvMain;
+	ArrayAdapter<String> adapter;
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// adapter
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,data);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapter = new ArrayAdapter<String>(this, R.layout.item,R.id.tvText, data);
+		gvMain = (GridView) findViewById(R.id.gvMain);
+		gvMain.setAdapter(adapter);
+		adjustGridView();
+	}
 
-		Spinner spinner = (Spinner) findViewById(R.id.spinner);
-		spinner.setAdapter(adapter);
-		// Title
-		spinner.setPrompt("Title");
-		//select item
-		spinner.setSelection(2);
-		// setup listener
-		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			@Override
-			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-				Toast.makeText(getBaseContext(),
-						"position " + i,
-						Toast.LENGTH_SHORT).show();
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> adapterView) {
-				//To change body of implemented methods use File | Settings | File Templates.
-			}
-		});
+	private void adjustGridView() {
+		//gvMain.setNumColumns(3);
+		gvMain.setNumColumns(GridView.AUTO_FIT);
+		gvMain.setColumnWidth(80);
+		gvMain.setVerticalSpacing(5);
+		gvMain.setHorizontalSpacing(2);
+		gvMain.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);// using free space
 	}
 }
